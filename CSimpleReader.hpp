3b3416a9b6d0775e01b600f99def09b86cb3557a
Aka <steiner.85ab@gmail.com>
2021-07-09 20:23:30 +0300

    format file CSimpleReader

--- CSimpleReader.hpp
+++ CSimpleReader.hpp
@@ -5,47 +5,43 @@ using namespace std;
 
 namespace NFile
 {
-   const char* getFile(const char* fileName, bool isLog = false)
+   const char *getFile(const char *fileName, bool isLog = false)
    {
-      char* retValue = 0;
+      char *retValue = 0;
 
-
-         fstream File(fileName);
-         if (File.is_open())
+      fstream File(fileName);
+      if (File.is_open())
+      {
+         int size = 0;
+         while (!File.eof())
          {
-            int size = 0;
-            while (!File.eof())
-            {
-               File.get();
-               ++size;
-            }
-
-            File.clear();
-            File.seekg(0);
-
-            retValue = new char[size];
-            retValue[size - 1] = 0;
-
-            File.read(retValue, size);
-
-            if (isLog)
-            {
-
-                  cout << "total readed " << size << endl;
-                  cout << "readed: " << File.gcount() << endl;
-                  cout << retValue << endl;
-
-               printf("File %s opened: %u byte were readed \n", fileName, size);
-            }
+            File.get();
+            ++size;
          }
-         else
+
+         File.clear();
+         File.seekg(0);
+
+         retValue = new char[size];
+         retValue[size - 1] = 0;
+
+         File.read(retValue, size);
+
+         if (isLog)
          {
+            cout << "total readed " << size << endl;
+            cout << "readed: " << File.gcount() << endl;
+            cout << retValue << endl;
 
-               printf("file %s not open \n", fileName);
-
+            printf("File %s opened: %u byte were readed \n", fileName, size);
          }
-         File.close();
-      
+      }
+      else
+      {
+         printf("file %s not open \n", fileName);
+      }
+      File.close();
+
       return retValue;
    }
 }
