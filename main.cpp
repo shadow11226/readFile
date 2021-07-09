be1a4d088b03d50bdfa621f21605ca983e590f60
Aka <steiner.85ab@gmail.com>
2021-07-09 20:35:13 +0300

    first test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "iostream"
+#include "CSimpleReader.hpp"
 
 using namespace std;
 
 int main()
 {
+   // usual test
+   cout << NFile::getFile("main.cpp", false);
+   
+   cin.get();
    return 0;
-}
+} 
